fb474ddb4b1745d791351549886558d43fba82fc
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2026-04-27 19:19:04 +0100

    chore: update miniapp sdk usage

--- mini-apps/src/wagmi.ts
+++ mini-apps/src/wagmi.ts
@@ -1,40 +1,13 @@
 import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector";
 import { http, createConfig } from "wagmi";
-import {
-  arbitrum,
-  avalanche,
-  base,
-  bsc,
-  hyperEvm,
-  mainnet,
-  monad,
-  optimism,
-  polygon,
-} from "wagmi/chains";
+import { base, mainnet } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [
-    arbitrum,
-    avalanche,
-    base,
-    bsc,
-    hyperEvm,
-    mainnet,
-    monad,
-    optimism,
-    polygon,
-  ],
+  chains: [base, mainnet],
   connectors: [miniAppConnector()],
   transports: {
-    [arbitrum.id]: http(),
-    [avalanche.id]: http(),
     [base.id]: http(),
-    [bsc.id]: http(),
-    [hyperEvm.id]: http(),
-    [optimism.id]: http(),
     [mainnet.id]: http(),
-    [monad.id]: http(),
-    [polygon.id]: http(),
   },
 });
 
